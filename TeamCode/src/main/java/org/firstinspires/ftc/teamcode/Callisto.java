6270efaae204e7e6a9b0df078ddfdce408ddd977
MoonshotsRobotics <moonshots@gilmour.org>
2024-09-13 12:13:18 -0400

    Callisto initial set up

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Callisto.java
@@ -0,0 +1,96 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.Robot;
+import com.arcrobotics.ftclib.command.button.Button;
+import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.commands.Drive;
+import org.firstinspires.ftc.teamcode.subsystems.Camera;
+import org.firstinspires.ftc.teamcode.subsystems.Mecanum;
+
+public class Callisto extends Robot {
+
+    // INSTANCE VARIABLES
+    public LinearOpMode opMode;
+    public GamepadEx player1;
+    public GamepadEx player2;
+
+    // SUBSYSTEMS
+    public Mecanum mecanum;
+
+    /**
+     * Welcome to the Command pattern. Here we assemble the robot and kick-off the command
+     * @param opMode The selected operation mode
+     */
+    public Callisto(LinearOpMode opMode) {
+        this.opMode = opMode;
+        player1 = new GamepadEx(opMode.gamepad1);
+        player2 = new GamepadEx(opMode.gamepad2);
+        initTele();
+    }
+
+    // OVERLOADED CONSTRUCTOR THAT RESPONDS TO AUTONOMOUS OPMODE USER QUERY
+//    public LilPrince(LinearOpMode opMode, boolean isRed, boolean isLeft, boolean goForBoard) {
+//        this.opMode = opMode;
+//    }
+
+    /**
+     * Set teleOp's default commands and player control bindings
+     */
+    public void initTele() {
+
+        // MECANUM SETUP
+        mecanum = new Mecanum(this);
+
+        // Register subsystems
+        // REGISTER THE SUBSYSTEM BEFORE THE DEFAULT COMMANDS
+        register(mecanum);
+
+        // Setting Default Commands
+        mecanum.setDefaultCommand(new Drive(this));
+
+
+
+        /*
+                .__                                      ____
+        ______  |  |  _____   ___.__.  ____ _______     /_   |
+        \____ \ |  |  \__  \ <   |  |_/ __ \\_  __ \     |   |
+        |  |_> >|  |__ / __ \_\___  |\  ___/ |  | \/     |   |
+        |   __/ |____/(____  // ____| \___  >|__|        |___|
+        |__|               \/ \/          \/
+        */
+        Button aButtonP1 = new GamepadButton(player1, GamepadKeys.Button.A);
+        aButtonP1.whenPressed(new InstantCommand(() -> {
+            mecanum.toggleFieldCentric();
+        }));
+
+        Button bButtonP1 = new GamepadButton(player1, GamepadKeys.Button.B);
+        bButtonP1.whenPressed(new InstantCommand(() -> {
+            mecanum.resetFieldCentricTarget();
+        }));
+
+        Button xButtonP1 = new GamepadButton(player1, GamepadKeys.Button.X);
+        Button yButtonP1 = new GamepadButton(player1, GamepadKeys.Button.Y);
+        Button dPadUpP1 = new GamepadButton(player1, GamepadKeys.Button.DPAD_UP);
+        Button dPadDownP1 = new GamepadButton(player1, GamepadKeys.Button.DPAD_DOWN);
+        Button dPadLeftP1 = new GamepadButton(player1, GamepadKeys.Button.DPAD_LEFT);
+        Button dPadRightP1 = new GamepadButton(player1, GamepadKeys.Button.DPAD_RIGHT);
+
+        /*
+                _                                    __
+               (_ )                                /'__`\
+         _ _    | |    _ _  _   _    __   _ __    (_)  ) )
+        ( '_`\  | |  /'_` )( ) ( ) /'__`\( '__)      /' /
+        | (_) ) | | ( (_| || (_) |(  ___/| |       /' /( )
+        | ,__/'(___)`\__,_)`\__, |`\____)(_)      (_____/'
+        | |                ( )_| |
+        (_)                `\___/'
+         */
+    }
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Camera.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.arcrobotics.ftclib.vision.AprilTag2dPipeline;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Callisto;
+import org.firstinspires.ftc.teamcode.util.HardwareNames;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+public class Camera {
+    // Instance variables
+    private boolean isAprilTag = true;
+
+    // Hardware
+    private Callisto m_robot;
+    private OpenCvCamera camera;
+
+    public Camera (Callisto robot, Telemetry telemetry) {
+        telemetry.addData("Camera Working", true);
+        telemetry.update();
+
+        // We instantiate the robot
+        m_robot = robot;
+
+        // Camera Instantiation procedures
+        int cameraMonitorViewId = m_robot.opMode.hardwareMap.appContext.getResources().
+                getIdentifier("cameraMonitorViewId", "id",
+                        m_robot.opMode.hardwareMap.appContext.getPackageName());
+
+        WebcamName webcamName = m_robot.opMode.hardwareMap.get(WebcamName.class, HardwareNames.WEBCAM_NAME);
+        camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);;
+
+        // FTC Dashboard Camera Set up stuff
+        FtcDashboard.getInstance().startCameraStream(camera, 60);
+
+        // Checks which pipeline we will use
+        if(isAprilTag) {
+            //Setting our pipeline to the camera
+            camera.setPipeline(new AprilTag2dPipeline());
+        }
+
+        // Opening the camera
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                // Usually this is where you'll want to start streaming from the camera (see section 4)
+                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+    }
+
+    // Method to determine which pipeline we will use
+    public void AprilTagPipelineSelected(boolean state) {
+        isAprilTag = state;
+    }
+}
